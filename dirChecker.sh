5d6d75e598863c5ecce38cb4a8915a04eafdacf5
Osama Momani <54727130+OsamaMomani@users.noreply.github.com>
2020-05-10 05:58:43 +0300

    Add files via upload

--- dirChecker.sh
+++ dirChecker.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 #   \----------------------------------.
 #   /   Directories Checker script      |
 #   \   version :   0.1                 |    
@@ -6,16 +6,16 @@
 #   \   CopyLift!                       |
 #   /-----------------------------------'
 
-# inisalize Style Sequences variables  (for Colors)
-NOCOLOR='\033[0m'   
+#	University ID : 134746
+#	Why this code is huge ? Because it works 100% in all Unix-like Shells (sh) as it only use tools that are available in all Unix-Like systems and does NOT require any other tools like (find,sort, etc). Also it has a STUNNING Styles. 
+
+# inisalize Style Sequences variables  (for Colors)  ((( NOTE : These variables are used in form of '${}' insted of '$'  to make the code more readalbe ))) 
+NOCOLOR='\033[0m'
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 BLUE='\033[0;34m'
 PURPLE='\033[0;35m'
-LIGHTGREEN='\033[1;32m'
 YELLOW='\033[1;33m'
-LIGHTBLUE='\033[1;34m'
-LIGHTCYAN='\033[1;36m'
 WHITE='\033[1;37m'
 #\/\//\/\/\/\/\/\//\/\/\/\/\/\//\/\/\/\/\/\//\/\/\/\/
 while true ; do  # Start Loop
@@ -30,11 +30,13 @@ echo "${BLUE}     ╚═╝ ┴  ${WHITE} ╚═╝└─┘┴ ┴┴ ┴┴ �
 
 #\/\//\/\/\/\/\/\//\/\/\/\/\/\//\/\/\/\/\/\//\/\/\/\/
 
-echo "${NOCOLOR}=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-==-=-=-=-="
+echo "=-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-==-=-=-=-=-=-=-=-==-=-=-=-${NOCOLOR}"
 if [ -z $1 ];then
-    echo "You Should Enter The File Path rel/abs ( ./dirChecker arg1 ) "; break ;
+    echo "${RED}You Should Enter The File Path rel/abs ${WHITE}( ./dirChecker arg1 ) 
+	${NOCOLOR}"; break ;
 elif [ $2 ];then
-    echo "Only 1 arg is Need ( ./dirChecker arg1 ) "; break ;
+    echo "${RED}Only 1 arg is Need ${WHITE}( ./dirChecker arg1 ) 
+	${NOCOLOR} "; break ;
 fi
 
 #--------
@@ -42,7 +44,7 @@ fi
 if [  -d "$1" ]; then
     echo "Selcted Path is ${YELLOW} $1" ; 
 else
-    echo "Directory not exist" ; break ;
+    echo "Directory not exist${NOCOLOR}" ; break ;
 fi
 
 case $1 in 
@@ -64,34 +66,39 @@ echo "${GREEN}   5.${WHITE} Exit (Ctrl+C)$IFS"
 read i
 case $i in 
     1)
-        echo $(find $1 -name *.sh)
+	echo "${GREEN}End with (.sh) "	
+        echo "${PURPLE} $(ls $1*.sh)"
         ;;
     2)
+	echo "${GREEN}Writable Files"
         for f in $1/* ;do  # all files in directory
             if [ "$(ls -l $f | cut -d ' ' -f 1 | grep 'w')"  ]; then   # if it has "w" on the fisrt column in ls -l
-                echo $f
+                echo "${PURPLE} $f "
             fi
         done 
         ;;
     3)
-        echo "      Largest file : $(ls -Ss |head -n 2 |tail -1 | cut -d ' ' -f 2) (${YELLOW}$(ls -Ss |head -n 2 |tail -1 | cut -d ' ' -f 1)${WHITE})KiloByte  ";
+	    echo "     ${GREEN} Largest file :${PURPLE} $(ls -Slash $1 |head -n 2 |tail -1 |rev| cut -d ' ' -f 1 |rev) (${YELLOW}$(ls -Slash $1 |head -n 2 |tail -1 | cut -d ' ' -f 1 )${WHITE})  ";  #reverse columns & text
         ;;
     4)
+	echo "Contain Digits"	
         if [ -z "$(ls $1/*[0-9]* 2> /dev/zero)" ];then  #incase of errors throw it to trash
-            echo "There are no files with digits" 
+            echo "${RED}There are no files with digits" 
         else 
             echo  "${GREEN}filse are : ${PURPLE}$IFS $(ls $1/*[0-9]* ) " 
         fi
         ;;
     5)
-        break; 
+        echo "Exiting ... ${NOCOLOR}"
+	sleep 1
+	break; 
         ;;
     *)
         echo "${RED} Wrong Choice, Try Again " ; sleep 1 ; continue
         ;;
 esac
 
-echo "$IFS ${GREEN}Press any key to try again, or ${RED} C to exit"
+echo "$IFS ${GREEN}Press any key to try again, or ${RED} C ${NOCOLOR} to exit"
 read a
 
 if [  "$a" = "C" -o "$a" = "c" ]; then
